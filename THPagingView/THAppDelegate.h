a57c6377a6bff2e76c8176ea7a4f938dd1445370
taberhuang <taber.huang@gmail.com>
2012-04-13 12:22:54 +0800

    Initial Commit

---
+++ THPagingView/THAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  THAppDelegate.h
+//  THPagingView
+//
+//  Created by Liang Huang on 12-4-13.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ THPagingView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  THPagingView
+//
+//  Created by Liang Huang on 12-4-13.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "THAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([THAppDelegate class]));
+    }
+}
